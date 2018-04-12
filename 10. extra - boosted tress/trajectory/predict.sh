e68fe08542bf6282408956a291f0b9d87d7ec290
Christian Ramsey <thechristianramsey@gmail.com>
2018-04-12 06:15:34 -0700

    removed boosted trees example

--- 10. extra - boosted tress/trajectory/predict.sh
+++
@@ -1,9 +0,0 @@
-
-gcloud ml-engine models create trajectory --regions us-central1 --enable-logging True
-gcloud ml-engine models list
-gcloud ml-engine versions list --model trajectory
-gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.4 --config ../config_predictions.yaml
-gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.4 
-gcloud ml-engine predict --model trajectory2 --version v1 --json-instances batch_predict.json
-
-
--- 10. extra - boosted tress/trajectory/predict_local.sh
+++
@@ -1,4 +0,0 @@
-gcloud ml-engine local predict --model-dir exporter/1520959794 --json-instances batch_predict.json
-
-
-    
