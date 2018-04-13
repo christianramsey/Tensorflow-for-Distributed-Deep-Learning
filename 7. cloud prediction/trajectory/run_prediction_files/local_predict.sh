6745c32dfec5c3ce25a11c42c84a50ee0d175ed4
Christian Ramsey <thechristianramsey@gmail.com>
2018-04-13 07:17:43 -0700

    Revert "repos disappeared, recovered them"
    This reverts commit c5e2ce824ae055f8992c3b217e0fdef9c0f9e148.

--- 7. cloud prediction/trajectory/run_prediction_files/local_predict.sh
+++ 7. cloud prediction/trajectory/run_prediction_files/local_predict.sh
@@ -1,1 +1,1 @@
-gcloud ml-engine local predict --model-dir ../output/export/exporter/1523459304 --json-instances ../run_prediction_files/batch_predict.json
+gcloud ml-engine local predict --model-dir ../output/export/exporter/1523377882 --json-instances ../run_prediction_files/batch_predict.json
