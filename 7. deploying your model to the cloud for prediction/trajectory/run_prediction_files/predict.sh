dc787accb5d0a126d1c12d3b686c5b827cddfff3
Christian Ramsey <thechristianramsey@gmail.com>
2018-04-15 08:16:18 -0700

    removed unks

--- 7. deploying your model to the cloud for prediction/trajectory/run_prediction_files/predict.sh
+++ 7. deploying your model to the cloud for prediction/trajectory/run_prediction_files/predict.sh
@@ -6,5 +6,3 @@ gcloud ml-engine versions list --model trajectory
 gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.6
 gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.6 --config config_predictions.yaml
 gcloud ml-engine predict --model trajectory2 --version v1 --json-instances batch_predict.json
-
-
