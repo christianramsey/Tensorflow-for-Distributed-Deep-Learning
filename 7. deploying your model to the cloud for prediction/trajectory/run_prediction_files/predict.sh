a78db891dbe9775898d7f45e55bfe2a89173cfbc
Christian Ramsey <thechristianramsey@gmail.com>
2018-04-15 08:15:25 -0700

    updated prediction runtime version

--- 7. deploying your model to the cloud for prediction/trajectory/run_prediction_files/predict.sh
+++ 7. deploying your model to the cloud for prediction/trajectory/run_prediction_files/predict.sh
@@ -1,9 +1,10 @@
 
-gcloud ml-engine models create trajectory --regions us-central1 --enable-logging True
+gcloud ml-engine models list
+gcloud ml-engine models create trajectory --regions us-central1 --enable-logging
 gcloud ml-engine models list
 gcloud ml-engine versions list --model trajectory
-gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.4 --config ../config_predictions.yaml
-gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.4 
+gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.6
+gcloud ml-engine versions create v2 --model trajectory2 --origin $MODEL_BINARY --runtime-version 1.6 --config config_predictions.yaml
 gcloud ml-engine predict --model trajectory2 --version v1 --json-instances batch_predict.json
 
 
