cbfd92d986c68d7a05d48684ea104b95bfb6c2c6
Christian Ramsey <thechristianramsey@gmail.com>
2018-04-22 08:44:03 -0700

    updated nvidia requirements for tensorflow 1.7+ and new read me

---
+++ 0. installation and setup/nvidia.sh
@@ -0,0 +1,61 @@
+
+
+# NVIDIA DISPLAY DRIVERS
+# http://www.linuxandubuntu.com/home/how-to-install-latest-nvidia-drivers-in-linux
+
+# Install the latest nvidia drivers
+
+sudo apt-get purge nvidia*
+sudo add-apt-repository ppa:graphics-drivers
+And update
+sudo apt-get update
+
+sudo apt-get install nvidia-384
+lsmod | grep nvidia
+lsmod | grep nouveau
+
+sudo apt-mark hold nvidia-384
+
+# Most recent driver: https://launchpad.net/~graphics-drivers/+archive/ubuntu/ppa
+# Restart  after you install nvidia drivers
+
+
+# INSTALL CUDA DRIVERS
+# Then start the cuda drivers and skip the display drivers install (use .sh)
+
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64\
+                     	${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+# Verify cuda drivers
+# http://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#post-installation-actions
+
+# deviceQuery
+
+# INSTALL CUDNN DRIVERS
+# Sign up for nvidia cudnn
+# https://developer.nvidia.com/rdp/cudnn-download
+
+cd folder/extracted/contents
+sudo cp -P include/cudnn.h /usr/include
+sudo cp -P lib64/libcudnn* /usr/lib/x86_64-linux-gnu/
+sudo chmod a+r /usr/lib/x86_64-linux-gnu/libcudnn*
+
+sudo cp -P include/cudnn.h /usr/local/cuda-9.0/include/
+sudo cp -P lib64/libcudnn* /usr/local/cuda-9.0/lib64/
+sudo chmod a+r /usr/local/cuda-9.0/lib64/libcudnn*
+
+# install command line tools and add to lib
+sudo apt-get install cuda-command-line-tools
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}/usr/local/cuda/extras/CUPTI/lib64
+
+
+# INSTALL SUPPORT LIB AND UPDATE
+sudo apt-get install libcupti-dev
+
+sudo apt-get update
+
+# If update has an issue due the following:
+sudo rm /etc/apt/apt.conf.d/50appstream
+# Source: https://askubuntu.com/questions/771547/ubuntu-16-04-apt-get-update-doesnt-work-with-local-repository
